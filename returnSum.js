f20f65fc7cd7bb5828f066b0b51af702ac1c2b38
Bruno Brinate <110791521+brunobrinate@users.noreply.github.com>
2023-03-20 21:49:08 -0300

    add returnSum and yearToCentury

---
+++ returnSum.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {number} number 
+ * @return {number}  
+ */
+
+const solution = ( param1 , param2) => {
+    let number1 = param1;
+    let number2 = param2;
+
+    let sum = number1 + number2;
+
+    return sum;
+}
+
+console.log(solution(1)(2));
