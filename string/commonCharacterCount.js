adce8dda34682c0ccdef186a37ffd89e2fada409
brunobrinate <bruno.brinate@hotmail.com>
2023-04-24 23:58:18 -0300

    remove error

--- string/commonCharacterCount.js
+++ string/commonCharacterCount.js
@@ -4,19 +4,17 @@
  */
 
 const solution = (s1,s2) =>{
-    const solution = (s1,s2) =>{
-        let counter = 0;
-        let arrayS1 = s1.split(``);
-        let y = s2;
-        while(arrayS1.length){
-         const x = arrayS1.pop();
-           if(y.includes(x)){
-             counter++;
-             y = y.replace(x,``);
-           }
-        }
-        return counter;
+  let counter = 0;
+  let arrayS1 = s1.split(``);
+  let y = s2;
+  while(arrayS1.length){
+   const x = arrayS1.pop();
+     if(y.includes(x)){
+       counter++;
+       y = y.replace(x,``);
      }
+  }
+  return counter;
 }
 
 console.log(solution(`aabcc`,`adcaa`));
