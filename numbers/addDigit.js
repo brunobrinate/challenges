7ba6a22161328baeb1ffa2a455b73a8ef990ce17
brunobrinate <bruno.brinate@hotmail.com>
2023-04-26 12:45:38 -0300

    add new challenge

---
+++ numbers/addDigit.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+
+
+const addDigits = num => {
+    let sum = 0;
+    let convert = String(num).split('');
+    for(let i = 0; i < convert.length; i++){
+    sum += parseInt(convert[i]);
+    }
+  
+    while(sum > 9){
+      let digits = String(sum).split('');
+      sum = 0;
+      for(let j = 0; j < digits.length; j++){
+      sum += parseInt(digits[j]); 
+      }
+    }
+    return sum;
+  }
+  
+  console.log(addDigits(38));
