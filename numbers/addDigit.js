a20fe4cb01ea26e3b8a51e042fc7d22275e01e5b
brunobrinate <bruno.brinate@hotmail.com>
2023-05-03 14:51:12 -0300

    update file

--- numbers/addDigit.js
+++ numbers/addDigit.js
@@ -5,20 +5,20 @@
 
 
 const addDigits = num => {
-    let sum = 0;
-    let convert = String(num).split('');
-    for(let i = 0; i < convert.length; i++){
-    sum += parseInt(convert[i]);
-    }
-  
-    while(sum > 9){
-      let digits = String(sum).split('');
-      sum = 0;
-      for(let j = 0; j < digits.length; j++){
-      sum += parseInt(digits[j]); 
-      }
-    }
-    return sum;
+  let sum = 0;
+  let convert = String(num).split('');
+  for(let i = 0; i < convert.length; i++){
+  sum += parseInt(convert[i]);
   }
-  
-  console.log(addDigits(38));
+
+  while(sum > 9){
+    let digits = String(sum).split('');
+    sum = 0;
+    for(let j = 0; j < digits.length; j++){
+    sum += parseInt(digits[j]); 
+    }
+  }
+  return sum;
+}
+
+console.log(addDigits(38));
