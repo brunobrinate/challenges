958110f32667b20e8d3466ac30674cf1d84cd08d
brunobrinate <bruno.brinate@hotmail.com>
2023-10-28 14:44:03 -0300

    update file

--- numbers/searchInsertPosition.js
+++ numbers/searchInsertPosition.js
@@ -5,9 +5,15 @@
  */
 
 const searchInsert = (nums,target) => {
-    
-}
-
-console.log(searchInsert([1,3,5,6],(5)));
-console.log(searchInsert([1,3,5,6],(2)));
-console.log(searchInsert([1,3,5,6],(7)));
+    let index = 0;
+    for(let i = 0; i < nums.length; i++){
+      if(target <= nums[i]) return index;
+      index++; 
+    }
+    return index;
+  }
+  
+  console.log(searchInsert([1,3,5,6],(5)));
+  console.log(searchInsert([1,3,5,6],(2)));
+  console.log(searchInsert([1,3,5,6],(7)));
+  
