95b6ba9ed8d9384fa1959099c5e13186b2f58f58
brunobrinate <bruno.brinate@hotmail.com>
2023-10-28 14:39:53 -0300

    add new challenge

---
+++ numbers/searchInsertPosition.js
@@ -0,0 +1,13 @@
+/**
+ * @param {array} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const searchInsert = (nums,target) => {
+    
+}
+
+console.log(searchInsert([1,3,5,6],(5)));
+console.log(searchInsert([1,3,5,6],(2)));
+console.log(searchInsert([1,3,5,6],(7)));
