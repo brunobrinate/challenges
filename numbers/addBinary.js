6106768d06a586e3a7425a72b99ee40c97049407
brunobrinate <bruno.brinate@hotmail.com>
2023-10-30 16:41:34 -0300

    add new challenge

---
+++ numbers/addBinary.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ * */
+
+const addBinary = (a,b) => {
+    
+};
+
+console.log(addBinary((11),(1)));
+console.log(addBinary((1010),(1011)));
