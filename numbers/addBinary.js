7411d74796a299e263d7c3ab4ca65f769d5f89a1
brunobrinate <bruno.brinate@hotmail.com>
2023-10-30 17:54:25 -0300

    update challenge

--- numbers/addBinary.js
+++ numbers/addBinary.js
@@ -5,7 +5,11 @@
  * */
 
 const addBinary = (a,b) => {
-    
+    const decimalNumber = parseInt(a,2);
+    const decimalNumber2 = parseInt(b,2);
+    let sum = decimalNumber + decimalNumber2;
+    const result = sum.toString(2);
+    return result;
 };
 
 console.log(addBinary((11),(1)));
